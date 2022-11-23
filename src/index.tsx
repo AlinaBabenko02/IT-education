304c593f4b349bed88e97f8dc76ecdf261db8313
Alina Babenko <your.alinaa@gmail.com>
2022-11-23 04:07:53 +0100

    feat: style

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { AppRoot } from "./app/index.jsx";
+import "./App.css";
 
 const rootElement = document.getElementById("root");
 export const App = () => (
